947119f30750af5ccdb4adef1387c318fcc4d71c
Juan G. Saurith M <juanguisam@gmail.com>
2023-09-09 19:57:26 -0500

    Update analisis file.

--- analisis.R
+++ analisis.R
@@ -3,4 +3,12 @@ library(TTR)
 library(dplyr)
 library(zoo)
 
-View(datos)
+datos <- arrange(datos, Date)  # Ordena por fecha ascendente
+ts_datos <- zoo(datos$Close, order.by = datos$Date)  # Crea una serie temporal
+
+
+mean_close <- mean(ts_datos)
+median_close <- median(ts_datos)
+sd_close <- sd(ts_datos)
+
+hist(ts_datos, main = "Distribución de Precios de Cierre", xlab = "Precio de Cierre", col = "blue")
